71613d19af66fff7d61b5f68a373acb6a92e71e7
dotLimitless <codeLimitless@gmail.com>
2020-10-24 09:20:37 +0300

    added config.py

--- app.py
+++ app.py
@@ -1,20 +1,52 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__)
-    CORS(app)
-
-    return app
-
-
+app = Flask(__name__)
+app.config.from_object('config')
+CORS(app)
 db = SQLAlchemy()
 
-APP = create_app()
+# TODO routes declaration
+
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "Unauthorized"
+    }), 401
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Not Found"
+    }), 404
+
+
+@app.errorhandler(500)
+def internal(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Internal Server Error"
+    }), 500
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request"
+    }), 400
+
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='localhost', port=8080)
