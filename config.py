fe93c073d9d1627da3cca5241520a475a80e6219
dotLimitless <codeLimitless@gmail.com>
2020-11-02 13:00:25 +0300

    finished heroku setup

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:root@localhost:5432/trivia'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:root@localhost:5432/capstone'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ import os
 import json
 import unittest
 from models.blog import setup_db, db, Blog
-from server import app
+from app import app
 
 # result = self.client().get('/questions?page=1')
 # result = self.client().post('/quizzes', json={
