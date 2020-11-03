ae247c6393a3d79e35b77efd137ee77860e44a85
dotLimitless <codeLimitless@gmail.com>
2020-11-03 14:32:25 +0300

    configured database and authentication

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:root@localhost:5432/capstone'
+SQLALCHEMY_DATABASE_URI = 'postgres://dlntzgezpcdfla:7398e6b6470f4b8a749e9d184e6433b872921170d71ce0b914407ffa263356a3@ec2-54-160-202-3.compute-1.amazonaws.com:5432/d296dsetq9a197'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- test_app.py
+++ test_app.py
@@ -19,7 +19,14 @@ from server import app
 class CapstoneTestCase(unittest.TestCase):
     """This class represents the capstone test case"""
     headers = {
-        'Authorization': os.environ.get('ADMIN_TOKEN')
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InZNYjVTYWF6bDVGUDczbjJtUWF5WCJ9\
+        .eyJpc3MiOiJodHRwczovL2Rldi1jaTV6NnpvNC5ldS5hdXRoMC5jb20vIiwic3ViIjoieUZlNEFvakFIdVdRekh3VWZQR1FkQzVKZ\
+        3dWVFBIRGhAY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3QvIiwiaWF0IjoxNjA0NDAyNDExLCJleHAiOjE2MDQ0ODg4M\
+        TEsImF6cCI6InlGZTRBb2pBSHVXUXpId1VmUEdRZEM1Smd3VlRQSERoIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.FDkK5m\
+        AHYAww-JO_S2wWnvSVnubsoblVuypEH89mCA6I4Pb6Xtw_I2bf0Mgg8dgkvr9pPN2Ke2VmyyiOrGb2wzji6hsetElI2uetXf2YcJqg\
+        _07bX_0AjRHZFHQy5otcbQEkQ6PUw8RPcH3jNgtoT4HYQyUJpeE7T3jG_ugqtMDRh7RhJikuDpTuczYA484tOkQfBKb0v_cGiDlo1E\
+        N5TQUsqKZq9QXwFyPxNvURNjS-f2hfd8v1bgC_BTEK2NmyEHrt5IfttC7ooSKH-g_wTb7Z6eGLkJiv4zIaqWF66kVUoOS54MWalrnt\
+        CyElk3c6CSPD2YJyIwwAJ_-1ZBG6rw'
     }
 
     def setUp(self):
@@ -27,9 +34,9 @@ class CapstoneTestCase(unittest.TestCase):
         self.app = app
         self.app.config['DEBUG'] = False
         self.client = self.app.test_client
-        self.database_name = "capstone_test"
-        self.database_path = f'postgres://postgres:root@\
-        localhost:5432/{self.database_name}'
+        self.database_path = 'postgres://jguyipenxdzxig:ef1a95cfcbc265ae17ad55d95f7\
+        ba4b794e1d03f58940c2df9f9c9216509b2e2@ec2-54-166-114-48.compute-1.amazonaws.\
+        com:5432/d9u9sernqgqat8'
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
